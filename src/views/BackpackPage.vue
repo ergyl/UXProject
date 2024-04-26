74206ffc8fdd509c4eb90ad569702c05cce85360
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-04-26 10:23:25 +0200

    flexbox added for buttons in BackpackPage

---
+++ src/views/BackpackPage.vue
@@ -0,0 +1,46 @@
+<!-- Page ? in the SPA -->
+
+<!--The MainLayout has defined a grid with 8 columns and 24 rows, 
+  which means the grid lines for columns are numbered from 1 to 9 
+  (since there are 8 columns, there will be 9 lines including 
+  the start and end lines), and for rows, the lines are from 1 to 25.
+
+  @col-start & end should be between 1-9, @col-start & end should be between 1-25-->
+
+<template>
+  <!-- Use the defined grid lines from MainLayout to position the content -->
+  <!-- Ensure that the column numbers here match the lines in the MainLayout grid -->
+  <div class="col-start-3 col-end-7 row-start-1 row-end-3 bg-red-200">
+    <!-- Content here will be placed within the MainLayout grid -->
+    Open backpack
+  </div>
+  <div class="col-start-1 col-end-9 row-start-3 row-end-3 text-center bg-green-200">
+    <p>Vilket föremål vill du läsa mer om?</p>
+  </div>
+
+  <div class="col-start-1 col-end-9 row-start-4 row-end-18 text-center bg-blue-500">
+    <p class="text-white">
+      Ryggsäck
+    </p>
+
+    <!-- Flex container wrapper positioned in the grid -->
+    <div class="grid grid-cols-8">
+      <div class="col-start-2 col-end-8">
+        <div class="flex">
+          <div class="w-1/2 bg-gray-400 h-12">
+            One
+          </div>
+          <div class="w-1/2 bg-gray-500 h-12">
+            Two
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+    
+    <script setup>
+    import { RouterLink } from 'vue-router';
+    import AppLogo from '../components/AppLogo.vue';
+    </script>
+    
---
+++ src/views/StoryPage.vue
@@ -0,0 +1,69 @@
+<!-- Page 2 in the SPA -->
+
+<template>
+  <div class="col-start-2 col-end-2 row-start-4 row-end-4 flex items-center justify-center">
+    <!-- Left arrow -->
+    <span
+      v-if="currentIndex > 0"
+      class="icon-[material-symbols-light--navigate-before] text-4xl cursor-pointer"
+      @click="goToPrevious"
+    />  
+  </div>
+
+  <div class="col-start-3 col-end-7 row-start-3 row-end-10 flex flex-col items-center">
+    <!-- Image -->
+    <img
+      :src="images[currentIndex]"
+      alt="Museum Building"
+      class="max-w-full h-auto"
+    >
+    <p class="text-center mt-4">
+      {{ storyText[currentIndex] }}
+    </p>
+
+    <RouterLink to="/home">
+      <button
+        v-if="currentIndex === images.length - 1"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4"
+      >
+        Spela
+      </button>
+    </RouterLink>
+  </div>
+  <div class="col-start-7 col-end-7 row-start-4 row-end-4 flex items-center justify-center">
+    <!-- Right arrow -->
+    <span
+      v-if="currentIndex < images.length - 1"
+      class="icon-[material-symbols-light--navigate-next] text-4xl cursor-pointer"
+      @click="goToNext"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { RouterLink } from "vue-router";
+import firstImage from "@/assets/images/illustrations/story-musem-building.png";
+import secondImage from "@/assets/images/illustrations/story-musem-visitors-border.png";
+import thirdImage from "@/assets/images/illustrations/mullward_left.png";
+
+const images = [firstImage, secondImage, thirdImage];
+const storyText = [
+  "Det var en gång ett fantastiskt museum...",
+  "..fullt av fascinerande historiska föremål och nyfikna besökare.",
+  "Nu är det din tur att utforska. Mullward behöver din hjälp!",
+];
+const currentIndex = ref(0);
+
+const goToPrevious = () => {
+  if (currentIndex.value > 0) {
+    currentIndex.value--;
+  }
+};
+
+const goToNext = () => {
+  if (currentIndex.value < images.length - 1) {
+    currentIndex.value++;
+  }
+};
+</script>
