70ff578428fd922ca4d3edfa8f1b7e143de8c15b
Ilja Grigorjev <tomkapisos@gmail.com>
2024-04-30 16:06:50 +0200

    Replacing black cover with dirt tiles

--- src/components/ui/NineCardsGrid.vue
+++ src/components/ui/NineCardsGrid.vue
@@ -6,7 +6,10 @@
       class="square border-2 border-black"
       @click="logIndex(index)"
     >
-      <div class="content flex items-center justify-center bg-black">
+      <div 
+        class="content flex items-center justify-center cover-image"
+        :style="clickedIndex === index ? {} : { backgroundImage: `url(${images[index]})` }"
+      >
         <img
           v-if="clickedIndex === index"
           :key="index"
@@ -14,13 +17,23 @@
           alt="Föremål"
           class="h-full w-auto object-cover"
         >
-        <div v-else class="h-full w-full object-cover bg-black"></div>
+        <div v-else class="h-full w-full object-cover"></div>
       </div>
     </button>
   </div>
 </template>
 
 <script>
+import image1 from '@/assets/images/illustrations/game/tile1.png';
+import image2 from '@/assets/images/illustrations/game/tile2.png';
+import image3 from '@/assets/images/illustrations/game/tile3.png';
+import image4 from '@/assets/images/illustrations/game/tile4.png';
+import image5 from '@/assets/images/illustrations/game/tile5.png';
+import image6 from '@/assets/images/illustrations/game/tile6.png';
+import image7 from '@/assets/images/illustrations/game/tile7.png';
+import image8 from '@/assets/images/illustrations/game/tile8.png';
+import image9 from '@/assets/images/illustrations/game/tile9.png';
+
 export default {
   name: 'NineCardsGrid',
   props: {
@@ -33,7 +46,8 @@ export default {
   },
   data() {
     return {
-      clickedIndex: null
+      clickedIndex: null,
+      images: [image1, image2, image3, image4, image5, image6, image7, image8, image9]
     }
   },
   methods: {
@@ -62,25 +76,8 @@ export default {
   align-items: center;
   justify-content: center;
 }
-</style>
-
-<style scoped>
-.square {
-  flex: 1 0 31%; /* slightly less than 1/3 to accommodate the gap */
-  height: 0;
-  padding-bottom: 31%; /* the same as width for maintaining aspect ratio */
-  position: relative;
-}
-.content {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+.cover-image {
+  background-size: cover;
+  background-position: center;
 }
 </style>
-
-  
