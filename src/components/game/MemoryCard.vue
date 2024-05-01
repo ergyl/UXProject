3fbef3506b957cfae97eab929e11ba4695a4eff5
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-05-01 22:53:22 +0200

    back image of memory cards working

---
+++ src/components/game/MemoryCard.vue
@@ -0,0 +1,64 @@
+<!-- Handles the state of the memory card and displays
+either front or back image-->
+
+<template>
+  <div
+    class="memory-card cursor-pointer content flex object-contain items-center justify-center"
+    @click="flip"
+  >
+    <img
+      v-if="!isFlipped"
+      :src="frontImage"
+      alt="Front"
+      class="item-thumbnail w-full h-auto"
+    >
+    <img
+      v-else
+      :src="backImage"
+      alt="Back"
+      class="item-thumbnail w-full h-auto"
+    >
+  </div>
+</template>
+  
+  <script>
+  import { useGameStore } from '@/stores/gameStore';
+
+  const gameStore = useGameStore();
+
+  export default {
+    name: 'MemoryCard',
+    props: {
+      frontImage: {
+        type: String,
+        default: '',
+      },
+      backImage: {
+        type: String,
+        default: '',
+      }
+    },
+    data() {
+      return {
+        isFlipped: false
+      };
+    },
+    methods: {
+      flip() {
+        this.isFlipped = !this.isFlipped;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .memory-card {
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+  }
+  .item-thumbnail {
+    backface-visibility: hidden;
+  }
+</style>
+
+  
---
+++ src/components/game/MemoryCardsGrid.vue
@@ -0,0 +1,43 @@
+<!-- Grid for the memory game, it is used to insert
+MemoryCard components in the grid -->
+
+<template>
+  <NineCardsGrid :items="frontImages">
+    <template #item-slot="{ item, index }">
+      <MemoryCard
+        :key="index"
+        :front-image="item"
+        :back-image="backImages[index]"
+        @click="logIndex"
+      />
+    </template>
+  </NineCardsGrid>
+</template>
+
+<script>
+import NineCardsGrid from '@/components/ui/NineCardsGrid.vue';
+import MemoryCard from './MemoryCard.vue';
+
+export default {
+  components: {
+    NineCardsGrid,
+    MemoryCard,
+  },
+  props: {
+    frontImages: {
+      type: Array,
+      default: () => [],
+    },
+    backImages: {
+      type: Array,
+      default: () => [],
+    }
+  },
+  methods: {
+      logIndex(index) {
+        this.clickedIndex = index;
+        console.log(index);
+      }
+    },
+};
+</script>
