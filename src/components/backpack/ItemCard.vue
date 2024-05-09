0a6362f46ea43d38b9341d6ef13e25c634954169
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-05-09 23:49:11 +0200

    new component ItemCard for backpackPage

---
+++ src/components/backpack/ItemCard.vue
@@ -0,0 +1,59 @@
+<!-- Image card to be put in BackpackCardsGrid -->
+
+<template>
+  <div
+    class="overlay flex items-center justify-center h-full w-full object-cover bg-cover bg-center cursor-pointer"
+    :class="{ active: isActive }"
+    :style="{ backgroundImage: `url(${frontImage})` }"
+    @click="toggle"
+  />
+</template>
+
+<script>
+export default {
+  name: 'ItemCard',
+  props: {
+    item: {
+      type: Object,
+      required: true
+    },
+    frontImage: {
+      type: String,
+      required: true
+    }
+  },
+  emits: ['select-item'],
+  data() {
+    return {
+      isActive: false
+    };
+  },
+  methods: {
+    toggle() {
+      this.isActive = !this.isActive;
+      this.$emit('select-item', this.item);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.overlay {
+  position: relative;
+}
+.overlay::after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(119, 158, 75, 0.7); /* Green color #779E4B with 50% opacity */
+  pointer-events: none;
+  display: none;
+}
+.overlay.active::after {
+  display: block;
+}
+</style>
+
