83b9513a4d9eab6b3c7261a50e38fcde7681f258
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-05-12 21:06:10 +0200

    new component PopUp and some new image files added

---
+++ src/components/ui/PopUp.vue
@@ -0,0 +1,28 @@
+<!-- A generic popup -->
+
+<template>
+  <div
+    class="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center"
+  >
+    <div 
+      class="min-w-64 max-w-72 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center text-center relative py-8"
+    >
+      <h3 class="mb-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {{ text }}
+      </h3>
+      <!-- Slot for custom content -->
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  },
+}
+</script>
