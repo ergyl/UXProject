35fad04614b8c8b00600ec0e2c64399b8190a19c
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-05-14 13:10:10 +0200

    Revert commit 23ec0b6
    This reverts commit 23ec0b6d09aad8999908841eb2239f05f995b9ca.

--- src/views/TipsPage.vue
+++ src/views/TipsPage.vue
@@ -1,13 +1,10 @@
 <!-- Page in the SPA -->
 
 <template>
-  <div class="overflow-scroll bg-green-500">
+  <div class="col-start-1 col-end-9 overflow-auto row-start-1 row-end-24">
     <div class="flex flex-col justify-start mt-10 ml-8 mr-8 border border-black bg-white">
-      <h2 class="mt-4 ml-4 self-center">
-        Skrivtips
-      </h2>
-      <p class="mt-4 ml-4 ">
-        Här är lite tips för att komma igång med att skriva om föremålen.
+      <p class="ml-4 mt-4">
+        Här är lite tips...
       </p>
       <fwb-accordion
         class="m-4"
@@ -37,8 +34,8 @@
                 Beskriv hur det påverkar din berättelse.
               </p>
               <p class="text-gray-500">
-                Vad <em>ser</em>, <em>hör</em>,
-                <em>smakar</em>, <em>luktar</em> eller <em>känner</em> karaktärerna?
+                Vad ser, hör,
+                smakar, luktar eller känner karaktärerna?
               </p>
             </div>
           </fwb-accordion-content>
@@ -85,14 +82,36 @@
         </fwb-accordion-panel>
       </fwb-accordion>
     </div>
+
+    <div class="ml-5">
+      <fwb-button
+        color="default"
+        size="lg"
+        class="m-4"
+        @click="goBack"
+      >
+        <template #prefix>
+          <span class="icon-[material-symbols-light--close] w-5 h-5 inline-block align-middle" />
+        </template>
+        Stäng
+      </fwb-button>
+    </div>
   </div>
 </template>
   
   <script setup>
+  import { useRouter } from 'vue-router';
   import {
   FwbAccordion,
   FwbAccordionContent,
   FwbAccordionHeader,
   FwbAccordionPanel,
+  FwbButton
 } from 'flowbite-vue'
+
+    const router = useRouter();
+
+    const goBack = () => {
+      router.back();
+    }
   </script>
