53ed08939f12e16547319b54b6e3d514d0d56291
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-05-15 00:44:50 +0200

    animation on pickaxe in BackpackPage when backpack is empty

--- src/components/ui/DigButton.vue
+++ src/components/ui/DigButton.vue
@@ -7,21 +7,36 @@
       :enabled-img="enabledImage"
       :is-active="isActive"
       :is-enabled="isEnabled"
-      :class="{ 'bg-beigebrun': isActive, 'bg-transparent': !isActive }"
+      :class="buttonClasses"
     />
   </div>
 </template>
   
   <script setup>
+  import { useBackpackStore } from '@/stores/backpackStore';
   import { computed, ref } from 'vue';
+  import { useRoute } from 'vue-router';
   import NavButton from './NavButton.vue';
   import activeImage from '@/assets/images/navmenu/icon_dig-active.png';
   import hoverImage from '@/assets/images/navmenu/icon_dig-hoverclick.png';
   import enabledImage from '@/assets/images/navmenu/icon_dig-enabled.png';
-  import { useRoute } from 'vue-router';
   
   const route = useRoute();
+  const backpackStore = useBackpackStore();
+
+  const isInEmptyBackpack = computed(() => {
+  return route.path === '/backpack' && backpackStore.spaceLeft === 9;
+});
+
   const isActive = computed(() => route.path === '/choose-category' || route.path === '/choose-difficulty' || route.path === '/game-play');
   const isEnabled = ref(true);
+
+  const buttonClasses = computed(() => ({
+  'bg-beigebrun': isActive.value,
+  'bg-transparent': !isActive.value,
+  'motion-safe:animate-bounce': isInEmptyBackpack.value,
+  'animate-none': !isInEmptyBackpack.value,
+}));
   </script>
+  
   
