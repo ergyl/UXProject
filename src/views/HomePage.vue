7203658340bbf8831e0b367dedfc8e4fd325e733
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-05-14 16:29:31 +0200

    Mullward image files added to HomePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,33 +5,34 @@ Check out https://www.tailwindgen.com/ to generate grid code :)
 Use Tailwind CSS by applying classes to html elements in class="..."-->
 
 <template>
-  <div class="grid grid-cols-8 grid-rows-2 overflow-scroll">
+  <div class="grid grid-cols-8 overflow-scroll">
     <div class="col-span-8 text-center">
-      <h2 class="mt-14 mb-2">
+      <h2 class="mt-14 mb-8">
         Hemma hos Mullward
       </h2>
     </div>
-    <div class="col-span-8 mt-6">
-      <p class="text-center">
-        [bild]
-      </p>
+    <div class="flex justify-center items col-span-8 bg-red-400">
+      <figure>
+        <img
+          :src="mullwardSmallImage" 
+          alt="Mullward sitter hemma i soffan"
+        >
+      </figure>
     </div>
   </div>
 </template>
 
 <script>
-import CategoryCard from "@/components/CategoryCard.vue";
+import mullwardSmallImage from '@/assets/images/illustrations/mullward_home-min.png'
+import mullwardBigImage from '@/assets/images/illustrations/mullward_home-max.png'
 
 export default {
   name: 'HomePage',
-  components: {
-    CategoryCard
-  },
+  data() {
+    return {
+      mullwardSmallImage,
+      mullwardBigImage
+    }
+  }
 };
 </script>
-
-<style scoped>
-.adjusted-width {
-  width: calc(50% - 0.75rem); /* Adjust the 12px to whatever half your gap-x value is */
-}
-</style>
