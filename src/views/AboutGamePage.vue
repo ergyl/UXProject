ce86a07cec7ab850503e0619975a01685976eeb7
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-05-15 01:18:45 +0200

    modified aboutGamePage with fun facts

--- src/views/AboutGamePage.vue
+++ src/views/AboutGamePage.vue
@@ -14,12 +14,26 @@
           Om spelet
         </fwb-accordion-header>
         <fwb-accordion-content>
-          <div>
-            <p class="mb-2 text-gray-500">
+          <div class="mb-2 text-gray-500">
+            <p class="mb-2">
+              <strong>Totalt 24 848 föremål att upptäcka.</strong>
+            </p>
+            <p class="mb-2">
               Kliv in i historiens mysterier som Mullward, en modig liten mullvad som
               tar på sig sig uppdraget att återställa ett tomt museum med historiska skatter.
+            </p>
+            <p class="mb-2">
               Upptäck spännande föremål och skapa egna äventyr genom att låta historien inspirera din fantasi!
             </p>
+            <p class="mb-2">
+              När du gräver fram föremål så får du fram nio slumpmässiga föremål. Så här många föremål finns i varje kategori:
+            </p>
+            <ul class="list-disc mb-2">
+              <li>Världen runt: 23 521 st</li>
+              <li>Leksaker:  752 st</li>
+              <li>Inramat: 575 st</li>
+            </ul>
+            <p>Möjligheterna är oändliga!</p>
           </div>
         </fwb-accordion-content>
       </fwb-accordion-panel>
@@ -28,10 +42,16 @@
           K-samsök
         </fwb-accordion-header>
         <fwb-accordion-content>
-          <div>
-            <p class="mb-2 text-gray-500">
-              Bilder och information om föremålen är hämtade från K-samsök. K-samsök tillgängliggör kulturarvsdata från svenska
-              museer och andra minnesinstitutioner. I K-samsök finns mer än tio miljoner objekt som till exempel museiföremål,
+          <div class="text-gray-500">
+            <p class="mb-2">
+              Bilder och information om föremålen är hämtade från K-samsök. 
+            </p>
+            <p class="mb-2">
+              K-samsök tillgängliggör kulturarvsdata från svenska
+              museer och andra minnesinstitutioner.
+            </p>
+            <p>
+              I K-samsök finns mer än tio miljoner objekt som till exempel museiföremål,
               lämningar, bildsamlingar och arkivhandlingar.
             </p>
           </div>
