28042ca58d870e67e8407eb5076efeac82aecd2c
fredriksjoback <fresjo0812@skola.goteborg.se>
2024-05-16 10:42:56 +0200

    Added progress-bar to landingpage

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,24 +1,35 @@
-<!-- Page in the SPA -->
-
-<!--The MainLayout has defined a grid with 8 columns and 24 rows, 
-  which means the grid lines for columns are numbered from 1 to 9 
-  (since there are 8 columns, there will be 9 lines including 
-  the start and end lines), and for rows, the lines are from 1 to 25.
-
-  @col-start & end should be between 1-9, @col-start & end should be between 1-25-->
-
 <template>
-  <div class="flex flex-row min-h-screen max-h-screen justify-center items-center">
-    <RouterLink
-      to="/story"
-    >
-      <AppLogo />
-    </RouterLink>
+  <div class="flex flex-col min-h-screen max-h-screen justify-center items-center">
+    <AppLogo class="h-16 w-auto" />
+  </div>
+  <div class="px-16 pt-6 pb-4">
+    <ProgressBar :progress="progress" :height="'5'" />
   </div>
 </template>
-  
-  <script setup>
-  import { RouterLink } from 'vue-router';
-  import AppLogo from '../components/AppLogo.vue';
-  </script>
-  
+
+<script setup>
+  import ProgressBar from '@/components/ProgressBar.vue';
+  import { useRouter } from 'vue-router';
+  import { ref } from 'vue';
+  import AppLogo from '@/components/AppLogo.vue';
+
+  const progress = ref('0');
+  const height = '8';
+  const router = useRouter();
+
+  const simulateProgress = () => {
+    let count = 0;
+    const interval = setInterval(() => {
+      count++;
+      progress.value = String(count * 5);
+      if (count === 20) {
+        clearInterval(interval);
+        setTimeout(() => {
+          router.push('/story');
+        }, 100);
+      }
+    }, 150);
+  };
+
+  simulateProgress();
+</script>
