26c2dbd05aef4cd28afe9624f366f38a85d3d0c9
fredriksjoback <fresjo0812@skola.goteborg.se>
2024-05-17 17:05:01 +0200

    Changed homepage so it changes image depending on screen size

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,38 +1,44 @@
 <!-- Page in the SPA -->
 
 <!-- The outer div in the template should be a grid container that places items in its grid layout
-Check out https://www.tailwindgen.com/ to generate grid code :) 
+Check out https://www.tailwindgen.com/ to generate grid code 🙂 
 Use Tailwind CSS by applying classes to html elements in class="..."-->
 
 <template>
-  <div class="grid grid-cols-8 overflow-scroll">
-    <div class="col-span-8 text-center">
-      <h2 class="mt-14 mb-8">
-        Hemma hos Mullward
-      </h2>
-    </div>
-    <div class="flex justify-center items col-span-8">
-      <figure>
-        <img
-          :src="mullwardSmallImage" 
-          alt="Mullward sitter hemma i soffan"
-        >
-      </figure>
-    </div>
+  <div class="grid grid-cols-8 overflow-auto lg:px-32 lg:pb-12">
+      <div class="col-span-8 text-center">
+          <h2 class="mt-14 mb-8">Hemma hos Mullward</h2>
+      </div>
+      <div class="flex justify-center items col-span-8">
+          <figure>
+              <img
+                  :src="image"
+                  alt="Mullward sitter hemma i soffan"
+              />
+          </figure>
+      </div>
   </div>
 </template>
 
-<script>
-import mullwardSmallImage from '@/assets/images/illustrations/mullward_home-min.png'
-import mullwardBigImage from '@/assets/images/illustrations/mullward_home-max.png'
+<script setup>
+import { ref, onMounted, computed, onUnmounted } from 'vue';
+import mullwardSmallImage from '@/assets/images/illustrations/mullward_home-min.png';
+import mullwardBigImage from '@/assets/images/illustrations/mullward_home-max.png';
 
-export default {
-  name: 'HomePage',
-  data() {
-    return {
-      mullwardSmallImage,
-      mullwardBigImage
-    }
-  }
+const image = computed(() =>
+  screenWidth.value > 768 ? mullwardBigImage : mullwardSmallImage
+);
+
+const screenWidth = ref(window.innerWidth);
+const updateScreenWidth = () => {
+  screenWidth.value = window.innerWidth;
 };
+
+onMounted(() => {
+  window.addEventListener('resize', updateScreenWidth);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateScreenWidth);
+});
 </script>
